72cfff393a1820cad27a40cbadddf37776018b89
= <170835528+FinalFenetix@users.noreply.github.com>
2025-12-19 00:14:50 +1000

    Added art for player and pipes as well as QOL features, tweaked vars

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,26 @@
 class_name Player
 extends CharacterBody2D
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export
 var jumpVel : float
 
-const gravity = 900
+@export
+var gravity = 900
+
+@export
+var startingVel = -250
+
+func _ready() -> void:
+	velocity.y = startingVel
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = jumpVel
+	
+	if velocity.y > 0:
+		sprite.play("falling")
+	else:
+		sprite.play("jumping")
 	
 	velocity.y += gravity * delta
 	move_and_slide()
